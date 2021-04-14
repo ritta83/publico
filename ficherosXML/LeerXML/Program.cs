c35f49b8e743d375715a481295b2c07b8f04bcff
ritta83 <ritamariamj@gmail.com>
2021-04-14 12:52:31 +0200

    xml
    xml

---
+++ ficherosXML/LeerXML/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Xml;
+
+namespace LeerXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("C:\\profesorcarlos.es\\1DAW-LLMM\\t3\\empleados.xml"); 
+            foreach (XmlNode node in doc.DocumentElement.ChildNodes)
+            {
+                String id = node.Attributes["id"].Value;
+                Console.WriteLine("id: " + id);
+                if (node.HasChildNodes)
+                {
+                    for (int i = 0; i < node.ChildNodes.Count; i++)
+                    {
+                        Console.WriteLine(node.ChildNodes[i].Name + " : " + node.ChildNodes[i].InnerText);
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
