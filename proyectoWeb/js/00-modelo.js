7bd013f518619b9ffdb263d3182cf519365598c0
ritta83 <ritamariamj@gmail.com>
2021-05-16 15:26:37 +0200

    act
    no se me actualiza no se si el enlace se abre bien.en mi github si aparecen los cambio.

--- proyectoWeb/js/00-modelo.js
+++ proyectoWeb/js/00-modelo.js
@@ -46,7 +46,7 @@ function miFuncion(xml) {
 
   if (comprobacion == true) {
     sessionStorage.setItem("usuario", nombre);
-    window.location.replace("leer.html");
+    window.location.replace("../paginas/leer.html");
   } else {
     window.alert("Usuario o contraseña incorrectos");
   }
